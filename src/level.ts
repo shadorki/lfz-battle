9200226f622c52226446bcacae4242c89b0f1457
Uzair Ashraf <uzinatorcl@live.com>
2020-12-14 23:39:02 -0800

    added debug mode for level and updated json file

--- src/level.ts
+++ src/level.ts
@@ -4,13 +4,34 @@ export default class Level {
   public root: HTMLElement
   public name: string
   public grid: any
-  constructor(name: string, root: HTMLElement) {
+  constructor(name: string, root: HTMLElement, isDebugMode: boolean = false) {
     this.root = root
     this.name = name
     this.grid = Level1
+    if(isDebugMode) {
+      this.triggerDebugView()
+    }
   }
-  isSpaceWalkable(x: number, y: number) {
+  isSpaceWalkable(x: number, y: number): boolean {
     if(!this.grid[`${x}/${y}`]) return false
     return !this.grid[`${x}/${y}`].isWalkable
+  }
+  triggerDebugView(): void {
+    for(const tile in this.grid) {
+      const { x, y, isWalkable} = this.grid[tile]
+      const element = document.createElement('div')
+      element.className = 'space'
+      element.style.top = `${y * 64}px`
+      element.style.left = `${x * 32}px`
+      element.textContent = `${x}/${y}`
+      if(isWalkable) {
+        element.style.border = '2px solid green'
+      } else {
+        element.style.border = '2px solid red'
+      }
+      this.root.append(element)
+    }
+    this.root.style.width = '1920px'
+    this.root.style.height = '1280px'
   }
 }
