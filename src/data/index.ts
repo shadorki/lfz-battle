b2cdb1486d1b131a39450a4744fe3f3483996bd7
Uzair Ashraf <uzinatorcl@live.com>
2020-12-21 14:04:33 -0800

    scott and timh added

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,7 +3,12 @@ import * as  pokemonCenter from './pokemon-center.json'
 import * as  gymEntrance from './gym-entrance.json'
 import * as  gymArena1PreBattle from './gym-arena-1-pre-battle.json'
 import * as  gymArena2PreBattle from './gym-arena-2-pre-battle.json'
+import * as  gymArena3PreBattle from './gym-arena-3-pre-battle.json'
+import * as  gymArena4PreBattle from './gym-arena-4-pre-battle.json'
 import * as  gymArena1PostBattle from './gym-arena-1-post-battle.json'
+import * as  gymArena2PostBattle from './gym-arena-2-post-battle.json'
+import * as  gymArena3PostBattle from './gym-arena-3-post-battle.json'
+import * as  gymArena4PostBattle from './gym-arena-4-post-battle.json'
 import * as  npcData from './npc.json'
 import * as  dialogueData from './dialogue.json'
 import * as  battleData from './battle.json'
@@ -14,7 +19,12 @@ const levels = {
   gymEntrance,
   gymArena1PreBattle,
   gymArena2PreBattle,
-  gymArena1PostBattle
+  gymArena3PreBattle,
+  gymArena4PreBattle,
+  gymArena1PostBattle,
+  gymArena2PostBattle,
+  gymArena3PostBattle,
+  gymArena4PostBattle,
 }
 
 export {
